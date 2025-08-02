470fff146a434604907a7c1588cd30a615b0b92f
Daniel <danilactpax@gmail.com>
2025-08-02 15:17:56 +0300

    Add UrlShortner to "main-container" & error messaging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import UrlShortner from "./UrlShortner";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [url, setUrl] = useState('');
+    const [error, setErrorMessage] = useState('');
+
+    const handleShortenClick = async () => {
+        if (!url) {
+            console.log("Введите ссылку");
+            setErrorMessage("Введите ссылку")
+            return;
+        }
+
+        let shorter = new UrlShortner();
+        console.log("Ссылка: ", url);
+
+        const shortUrlResult = await shorter.getShortUrl(url);
+
+        if (shortUrlResult) {
+            console.log("Короткая ссылка:", shortUrlResult);
+            setErrorMessage('');
+        } else {
+            console.log("Не удалось создать короткую ссылку");
+            setErrorMessage("Не удалось создать короткую ссылку");
+        }
+    }
+
+
+    return (
+        <div className="app">
+            <div className="main-container">
+                <input
+                    type={'url'}
+                    className="url-input"
+                    placeholder="Put your URL"
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                />
+                <button className="button" onClick={handleShortenClick}>
+                    Add
+                </button>
+            </div>
+            <div className="error-container">{error}</div>
+        </div>
+    );
 }
 
 export default App;
