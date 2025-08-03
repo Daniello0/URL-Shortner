f7026ca1a1862d652fd1cf3add31dee2960504c2
Daniel <danilactpax@gmail.com>
2025-08-03 15:25:12 +0300

    add IpInfo class and "get ip" method, tests

--- src/test/ParsingData.test.js
+++ src/test/ParsingData.test.js
@@ -1,17 +1,26 @@
 import UAParser from "ua-parser-js";
+import IpInfo from "../../IpInfo";
 
 describe('UserAgent', () => {
     test("Тестирование работы UserAgent", () => {
         const userAgent = navigator.userAgent;
         console.log("Данные от User-Agent: " + userAgent);
 
-        const parser = new UAParser();
 
+        // Более простой способ - через "ua-parser-js"
+        const parser = new UAParser();
         const result = parser.getResult();
         console.log(result);
     })
 })
 
 describe('getIP', () => {
-
+    test("получение IP адреса", async () => {
+        let data;
+        let ipInfo = new IpInfo();
+        data = await ipInfo.getIpInfo();
+        if (data) {
+            console.log(data);
+        }
+    })
 })
