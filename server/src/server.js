86a8b597dbef509fe34fbeb88762712aab00f13c
Daniel <danilactpax@gmail.com>
2025-08-12 12:48:12 +0300

    add connected DB, add addTestData.js (create test rows in DB), add ServerController.ts (test client-server connections)

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const cors = require('cors'); // 1. Импортируем cors
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
 
-// 2. Используем middleware cors. Теперь наш сервер разрешает запросы с других доменов.
+app.use(express.json());
 app.use(cors());
+
+app.post('/api/add', (req, res) => {
+    const { url, shortUrlIndex, userStatistic } = req.body;
+    const data = { url, shortUrlIndex, userStatistic };
+    console.log("Данные с клиента получены:\n", JSON.stringify(data, null, 2));
+
+    res.send("Данные успешно получены на сервере!");
+});
 
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Привет от бэкенда! Все работает.' });
