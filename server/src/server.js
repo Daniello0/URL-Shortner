0c00aee6253e395946a1c8f18dd2f47521596bb1
Daniel <danilactpax@gmail.com>
2025-08-12 10:17:40 +0300

    add server, project refactoring

---
+++ server/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors'); // 1. Импортируем cors
+const app = express();
+const PORT = 3001;
+
+// 2. Используем middleware cors. Теперь наш сервер разрешает запросы с других доменов.
+app.use(cors());
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Привет от бэкенда! Все работает.' });
+});
+
+app.post('/api/create-short-url', (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Бэкенд-сервер запущен на http://localhost:${PORT}`);
+});
